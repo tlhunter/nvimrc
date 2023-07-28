d3bae0ddf35f182823ed49aadf8553f219e2fe67
Thomas Hunter II <me@thomashunter.name>
2023-07-27 21:32:27 -0700

    lazyvim init

---
+++ init.lua
@@ -0,0 +1,9 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- disable relative line numbers
+vim.wo.relativenumber = false
+
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,15 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- disable <Escape j/k> moving lines, makes it clumsy to do things like leave input mode
+vim.api.nvim_del_keymap("n", "<A-j>")
+vim.api.nvim_del_keymap("n", "<A-k>")
+vim.api.nvim_del_keymap("i", "<A-j>")
+vim.api.nvim_del_keymap("i", "<A-k>")
+vim.api.nvim_del_keymap("v", "<A-j>")
+vim.api.nvim_del_keymap("v", "<A-k>")
+
+-- my favorite habit
+vim.api.nvim_set_keymap("n", "H", "^", { desc = "Start of line" })
+vim.api.nvim_set_keymap("n", "L", "$", { desc = "End of line" })
